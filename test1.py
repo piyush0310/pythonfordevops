a129a0413bc92119edcb453c47a7744431398753
piyush0310 <piyushkumar8256@gmail.com>
2024-07-10 22:27:05 +0530

    sc

---
+++ test1.py
@@ -0,0 +1,9 @@
+import requests
+
+response = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")
+
+complete_detail = response.json()
+
+for i in range(len(complete_detail)):
+
+    print(complete_detail[1] ["user"] ["id"])
